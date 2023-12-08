0f3b84bd8dcce8ae03e0a3040045d4fcad7bb014
YannQgn <yannqueguiner29@hotmail.fr>
2023-12-08 10:31:28 +0100

    update login + add register(+route)

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def login():
     return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 @app.route('/dashboard')

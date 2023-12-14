075d90a56c5872d5e67eb46e4c3598a4197118a8
redhox <morgan.c@neuf.fr>
2023-12-14 15:32:19 +0100

    tocken parout

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def check_authentication(func):
 @app.route('/', methods=['GET', 'POST'])
 def login():
     response = requests.get(f'{SERVER_URL}/check_db_empty')
+    
     response = response.json()
     print(response["message"])
     if response["message"] == 'True':
@@ -176,46 +177,57 @@ def dashboard():
 @app.route('/labelling', methods=['GET'])
 @check_authentication
 def labelling():
-    id_image = request.args.get('id_image', None)
-    info_image = requests.get(f'{SERVER_URL}/affiche_image/{id_image}')
-    data_image = info_image.json()
-    filepath = data_image['result']['path']
-    filepath = {"filepath": filepath}
-    response_annotation = requests.post(
-        f'http://equipe2.lumys.tech:5005/load_images', json=filepath)
-    data = response_annotation.json()
-    filename = [image_info['filename']
-                for image_info in data['images'].values()]
-    filename = filename[0]
-    print(f"Image:{filename}")
+    token = session.get('token')
+    if token:
+        headers = {'Authorization': f'JWT {token}'}
+        id_image = request.args.get('id_image', None)
+        info_image = requests.get(f'{SERVER_URL}/affiche_image/{id_image}', headers=headers)
+        
+        data_image = info_image.json()
+        filepath = data_image['result']['path']
+        filepath = {"filepath": filepath}
+        response_annotation = requests.post(f'http://equipe2.lumys.tech:5005/load_images', json=filepath , headers=headers)
+        
+        data = response_annotation.json()
+        filename = [image_info['filename']
+                    for image_info in data['images'].values()]
+        filename = filename[0]
+        print(f"Image:{filename}")
 
-    response_image = requests.post(
-        f'{SERVER_URL}/image_from_server', json=filepath)
+        response_image = requests.post(f'{SERVER_URL}/image_from_server', json=filepath , headers=headers)
 
-    with open(f"./image/{filename}", 'wb') as f:
-        f.write(response_image.content)
-    image = filename
-    return render_template('labelling.html', image=image, filename=filename, data=data, id_image=id_image)
+        with open(f"./image/{filename}", 'wb') as f:
+            f.write(response_image.content)
+        image = filename
+        return render_template('labelling.html', image=image, filename=filename, data=data, id_image=id_image)
+    else:
+        print("Authentication failed")
+        return jsonify({'message': 'Authentication failed'}), 401
 
 
 @app.route('/resultat', methods=['POST'])
 def resultat():
     print("resultat")
+    token = session.get('token')
 
-    # Récupérer le fichier du formulaire multipart
-    uploaded_file = request.files['file']
-    file_content = uploaded_file.read().decode('utf-8')
-    id_value = request.form['id']
-    # response = requests.post(f'{SERVER_URL}/post_resultat/{id_value}')
-    file_content = json.loads(file_content)
-    # print(list(file_content.keys())[0])
-    data = {'id': id_value, 'regions': file_content[list(file_content.keys())[
-        0]]["regions"]}
-    print('uplod', data)
-    response = requests.post(f'{SERVER_URL}/post_resultat', json=data)
-    response_json = response.json()
-    return jsonify({'status': 'success', 'message': 'File uploaded successfully'})
-
+    if token:
+        headers = {'Authorization': f'JWT {token}'}
+        # Récupérer le fichier du formulaire multipart
+        uploaded_file = request.files['file']
+        file_content = uploaded_file.read().decode('utf-8')
+        id_value = request.form['id']
+        # response = requests.post(f'{SERVER_URL}/post_resultat/{id_value}')
+        file_content = json.loads(file_content)
+        # print(list(file_content.keys())[0])
+        data = {'id': id_value, 'regions': file_content[list(file_content.keys())[
+            0]]["regions"]}
+        print('uplod', data)
+        response = requests.post(f'{SERVER_URL}/post_resultat', json=data , headers=headers)
+        response_json = response.json()
+        return jsonify({'status': 'success', 'message': 'File uploaded successfully'})
+    else:
+            print("Authentication failed")
+            return jsonify({'message': 'Authentication failed'}), 401
 
 @app.route('/get_image/<filename>')
 def get_image(filename):
@@ -231,13 +243,18 @@ def get_image(filename):
 
 
 @app.route('/overview')
-@check_authentication
+#@check_authentication
 def overview():
-    response = requests.get(f'{SERVER_URL}/find_all_image')
-    response.raise_for_status()  # Gère les erreurs HTTP
-    images = response.json()  # Convertit la réponse JSON en Python dict
-    return render_template('overview.html', images=images)
-
+    token = session.get('token')
+    if token:
+        headers = {'Authorization': f'JWT {token}'}
+        response = requests.post(f'{SERVER_URL}/find_all_image' , headers=headers)
+        response.raise_for_status()  # Gère les erreurs HTTP
+        images = response.json()  # Convertit la réponse JSON en Python dict
+        return render_template('overview.html', images=images )
+    else:
+        print("Authentication failed")
+        return jsonify({'message': 'Authentication failed'}), 401
 
 @app.route('/help')
 @check_authentication
@@ -248,16 +265,22 @@ def help():
 @app.route('/admin/user_management')
 @check_authentication
 def user_management():
-    try:
-        # Remplacez l'URL par l'URL réelle de votre API pour récupérer les utilisateurs
-        response = requests.get(f'{SERVER_URL}/all_user')
-        response.raise_for_status()  # Gère les erreurs HTTP
-        users = response.json()  # Convertit la réponse JSON en Python dict
-        return render_template('user_management.html', users=users)
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching users: {e}")
-        return []
+    token = session.get('token')
 
+    if token:
+        headers = {'Authorization': f'JWT {token}'}
+        try:
+            # Remplacez l'URL par l'URL réelle de votre API pour récupérer les utilisateurs
+            response = requests.get(f'{SERVER_URL}/all_user' , headers=headers)
+            response.raise_for_status()  # Gère les erreurs HTTP
+            users = response.json()  # Convertit la réponse JSON en Python dict
+            return render_template('user_management.html', users=users)
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching users: {e}")
+            return []
+    else:
+        print("Authentication failed")
+        return jsonify({'message': 'Authentication failed'}), 401
 
 @app.route('/admin/delete_user/<int:user_id>', methods=['POST'])
 @check_authentication
@@ -266,8 +289,7 @@ def delete_user(user_id):
     if token:
         try:
             headers = {'Authorization': f'JWT {token}'}
-            response = requests.post(
-                f'{SERVER_URL}/delete_user/{user_id}', headers=headers)
+            response = requests.post(f'{SERVER_URL}/delete_user/{user_id}', headers=headers)
             response.raise_for_status()
             return render_template('user_management.html'), 200
         except requests.exceptions.RequestException as e:
@@ -284,15 +306,13 @@ def switch_role(user_id):
     if token:
         try:
             headers = {'Authorization': f'JWT {token}'}
-            response = requests.get(
-                f'{SERVER_URL}/user/{user_id}', headers=headers)
+            response = requests.get(f'{SERVER_URL}/user/{user_id}', headers=headers)
             response.raise_for_status()
             user_data = response.json().get('user')
             current_role = user_data.get('role')
             new_role = 'admin' if current_role == 'utilisateur' else 'utilisateur'
             data = {'role': new_role}
-            response = requests.post(
-                f'{SERVER_URL}/change_role/{user_id}', json=data, headers=headers)
+            response = requests.post(f'{SERVER_URL}/change_role/{user_id}', json=data, headers=headers)
             response.raise_for_status()
             return render_template('user_management.html'), 200
         except requests.exceptions.RequestException as e:
